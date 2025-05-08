b44124b00a706395fe7bc0c5ea5cc186b5f8fceb
Samson Yichalal <samson.yichalal1@gmail.com>
2025-05-08 02:02:48 -0700

    Add AreaOfRectangle.java

---
+++ Java/Java-basics/AreaOfRectangle.java
@@ -0,0 +1,12 @@
+// Calculate the Area of a Rectangle
+public class AreaOfRectangle {
+    public static void main(String[] args){
+
+        int length = 6;
+        int width = 7;
+        int area = length * width;
+
+
+        System.out.println("The area of rectangle width " + width + " and length " + length + " is " + area);
+    }
+}
