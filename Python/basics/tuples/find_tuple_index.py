603329d85ba154cde9cc73fa793d50a477a68439
samson yichalal <samson.yichalal1@gmail.com>
2026-03-16 22:44:38 +0300

    Add program to find tuple index

---
+++ Python/basics/tuples/find_tuple_index.py
@@ -0,0 +1,8 @@
+# This program finds the position
+# of an item inside a tuple.
+
+countries = ("Ethiopia", "Kenya", "Sudan", "Uganda")
+
+index_of_sudan = countries.index("Sudan")
+
+print("Index of Sudan:", index_of_sudan)
