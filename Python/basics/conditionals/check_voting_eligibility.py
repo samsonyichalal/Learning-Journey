571182ade299585b2202ce1419929021bb8a0477
samson yichalal <samson.yichalal1@gmail.com>
2026-03-18 18:15:00 +0300

    Add program to check voting eligibility

---
+++ Python/basics/conditionals/check_voting_eligibility.py
@@ -0,0 +1,9 @@
+# This program checks whether a person
+# is old enough to vote.
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
